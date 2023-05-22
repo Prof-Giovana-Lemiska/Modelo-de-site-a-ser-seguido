2373a867d5b3151808fa715ab4cfd6f48027167e
Prof-Giovana-Lemiska <130910854+Prof-Giovana-Lemiska@users.noreply.github.com>
2023-05-22 17:11:37 -0200

    j

--- processar_formulario.php
+++ processar_formulario.php
@@ -1,15 +1,21 @@
 <?php
-$para = "lemiska.giovana@escola.pr.gov.br";
-$assunto = "Nova mensagem do formulário de contato";
-$remetente = $_POST['email'];
-$nome = $_POST['nome'];
-$mensagem = $_POST['mensagem'];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $para = "seu_email@example.com";
+    $assunto = "Nova mensagem do formulário de contato";
+    $remetente = $_POST['email'];
+    $nome = $_POST['nome'];
+    $mensagem = $_POST['mensagem'];
 
-$mensagem_formatada = "Nome: $nome\n";
-$mensagem_formatada .= "E-Mail: $remetente\n\n";
-$mensagem_formatada .= "Mensagem:\n$mensagem";
+    $mensagem_formatada = "Nome: $nome\n";
+    $mensagem_formatada .= "E-Mail: $remetente\n\n";
+    $mensagem_formatada .= "Mensagem:\n$mensagem";
 
-$headers = "From: $remetente";
+    $headers = "From: $remetente";
 
-mail($para, $assunto, $mensagem_formatada, $headers);
+    mail($para, $assunto, $mensagem_formatada, $headers);
+
+    // Redireciona o usuário para uma página de confirmação
+    header('Location: confirmacao.html');
+    exit();
+}
 ?>
