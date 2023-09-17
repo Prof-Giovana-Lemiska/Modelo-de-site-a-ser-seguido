604698e628f961732ae1b207a769c253eb8a0fc3
Prof-Giovana-Lemiska <130910854+Prof-Giovana-Lemiska@users.noreply.github.com>
2023-09-17 20:53:36 -0300

    f

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function tocar (idElementoAudio) {
+function tocar(idElementoAudio) {
     document.querySelector(idElementoAudio).play();
 }
 
@@ -7,11 +7,16 @@ const ListaTeclas = document.querySelectorAll('.tecla');
 let Contador = 0;
 
 while (Contador < ListaTeclas.length) {
+
     const tecla = ListaTeclas[Contador].classList[1];
-    console.log(tecla)
-ListaTeclas[Contador].onclick = function () {
-tocar
-};
+
+
+    const idAudio = `#som_${tecla} `
+    console.log(idAudio);
+
+    ListaTeclas[Contador].onclick = function () {
+        tocar
+    };
     Contador = Contador + 1;
     console.log(Contador);
 }
