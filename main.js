4233c83bb45336e0e46f88b2facae6ed7e6cf5b5
Prof-Giovana-Lemiska <130910854+Prof-Giovana-Lemiska@users.noreply.github.com>
2023-09-24 21:50:27 -0300

    d

--- main.js
+++ main.js
@@ -7,22 +7,23 @@ const ListaTeclas = document.querySelectorAll('.tecla');
 let Contador = 0;
 
 while (Contador < ListaTeclas.length) {
+    const botão = ListaTeclas[Contador];
+    const tecla = botão.classList[1];
 
-    const tecla = ListaTeclas[Contador].classList[1];
 
 
     const idAudio = `#som_${tecla} `
-    ListaTeclas[Contador].onclick = function () {
+    botão.onclick = function () {
         tocar(idAudio);
     }
 
     Contador = Contador + 1;
 
-    ListaTeclas[Contador].onkeydown = function() {
-        ListaTeclas[Contador].classList.add('ativa');
+    botão.onkeydown = function() {
+        botão.classList.add('ativa');
     }
-    ListaTeclas[Contador].onkeyup = function() {
-        ListaTeclas[Contador].classList.add('ativa');
+    botão.onkeyup = function() {
+        botão.classList.add('ativa');
     }
     }
 
